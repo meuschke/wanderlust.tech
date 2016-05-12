51b7872df808f37750971cdada13de170a0a7aba
Toni Meuschke <toni.meuschke@googlemail.com>
2016-05-12 11:04:16 +0900

    Deploy

--- deploy.sh
+++
@@ -1,5 +0,0 @@
-NODE_ENV=production gatsby build
-cp static/* public
-git add .
-git commit -am "Deploy"
-git subtree push --prefix public origin gh-pages
--- utils/url.js
+++
@@ -1,9 +0,0 @@
-import url from 'url'
-
-exports.absolutePath = function(path){
-
-  const URI = url.parse(path)
-
-  return "/" + URI.pathname.replace(/^\//, "")
-
-}
