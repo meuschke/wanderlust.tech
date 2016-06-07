e80ab5344aa6b15e001eefcb4b5efae7c1980ff1
Toni Meuschke <toni.meuschke@googlemail.com>
2016-06-07 15:53:15 +0800

    Deploy

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,14 +1,12 @@
 module.exports = {
   modifyWebpackConfig: function (config, env) {
+    console.log("Change Image Loader")
     config.removeLoader('jpg')
-    config.loader('jpg', {
-      test: /\.(jpe?g)$/i,
-      loaders: [
-        'file?hash=sha512&digest=hex&name=[hash].[ext]',
-        'image-webpack?bypassOnDebug&optimizationLevel=7&interlaced=false'
-      ]
-    }
-    )
+    config.loader('jpg', function(cfg) {
+      cfg.test = /\.jpg$/i
+      cfg.loader = 'file?hash=sha512&digest=hex&name=[hash].[ext]!image-webpack?optimizationLevel=3&interlaced=false'
+      return cfg
+    })
     return config
   },
   rewritePath: function (parsedPath) {
